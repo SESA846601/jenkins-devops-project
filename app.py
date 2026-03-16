dabd959c0803c1959754037a11df597703c5687f
SESA846601 <SESA846601@se.com>
2026-03-16 09:51:08 +0530

    testing pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps CI/CD Pipeline Working 🚀 checking if pipeline is working"
+    return "DevOps CI/CD Pipeline Working 🚀 checking if pipeline is working testing"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
